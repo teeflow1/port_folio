658a8efff08108cd61f36f52f5c800a1399e3e65
--add <temtopeayobami@gmail.com>
2023-08-06 18:42:34 +0100

    Done

--- app_folder/views.py
+++ app_folder/views.py
@@ -7,7 +7,7 @@ def home(request):
         email = request.POST['email']
         subject = request.POST['subject']
         message = request.POST['message']
-        
+        '''
         send_mail(
            'message from ' + name, #Subject
             message, # message
@@ -15,6 +15,8 @@ def home(request):
             email, # from email
            ['temitopeayobami995@gmail.com'], # to email
         )
+        '''
+        send_mail(name, message, subject, email, ['temitopeayobami995@gmail.com'])
         return render(request, 'apps/home.html', {'name':name})
     
     else:
