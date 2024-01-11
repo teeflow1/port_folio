0c8b3680219eb05491f64dc6db6e8541aedab19e
-add <temtopeayobami@gmail.com>
2024-01-11 17:32:27 +0100

    Done

--- project/settings.py
+++ project/settings.py
@@ -168,5 +168,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
